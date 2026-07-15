afddb269ebc5ad62a89feab0d677f650e86dc912
BharadwajShivanya <bharadwajtwinkle14@gmail.com>
2026-07-15 10:35:17 +0530

    Update

--- test_prompt.py
+++ test_prompt.py
@@ -1,6 +1,7 @@
 from parser import parse_trivy_report
 from statistics import calculate_statistics
 from prioritizer import get_top_vulnerabilities
+from summary_builder import build_summary
 from prompts import build_security_prompt
 
 findings = parse_trivy_report(
@@ -11,9 +12,12 @@ stats = calculate_statistics(findings)
 
 top_findings = get_top_vulnerabilities(findings)
 
-prompt = build_security_prompt(
+summary = build_summary(
+    findings,
     stats,
     top_findings,
 )
+
+prompt = build_security_prompt(summary)
 
 print(prompt)
